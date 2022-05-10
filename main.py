89915d6da6f7ce2538b89c245d3c26898e6287a2
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-05-10 16:13:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,142 @@
+     
+## This is a addon to the choco package manager for personal downloads.(apps that arent available)
+
+import requests
+from time import sleep
+import keyboard as kb
+import os
+from pystyle import Write, Colorate, Colors
+import getpass
+import random
+
+
+username = getpass.getuser()
+
+
+external = '''
+
+{external}
+
+'''
+
+internal = '''
+
+{internal}
+
+'''
+
+banner = '''
+
+            ee...      .....       .eeec.   ..eee..
+          .d*"  """"*e..d*"""""**e..e*""  "*c.d""  ""*e.
+         z"           "$          $""       *F         **e.
+        z"             "c        d"          *.           "$.
+       .F                        "            "            'F
+       d                                                   J%
+       3         .                                        e"
+       4r       e"              .                        d"
+        $     .d"     .        .F             z ..zeeeeed"
+        "*beeeP"      P        d      e.      $**""    "
+            "*b.     Jbc.     z*%e.. .$**eeeeP"
+               "*beee* "$$eeed"  ^$$$""    "
+                        '$$.     .$$$c
+unofficialdxnny          "$$.   e$$*$$c   (Danial Ahmed)
+                          "$$..$$P" '$$r
+                           "$$$$"    "$$.           .d
+               z.          .$$$"      "$$.        .dP"
+               ^*e        e$$"         "$$.     .e$"
+                 *b.    .$$P"           "$$.   z$"
+                  "$c  e$$"              "$$.z$*"
+                   ^*e$$P"                "$$$"
+                     *$$                   "$$r
+                     '$$F                 .$$P
+                      $$$                z$$"
+                      4$$               d$$b.
+                      .$$%            .$$*"*$$e.
+                   e$$$*"            z$$"    "*$$e.
+                  4$$"              d$P"        "*$$e.
+                  $P              .d$$$c           "*$$e..
+                 d$"             z$$" *$b.            "*$L
+                4$"             e$P"   "*$c            ^$$
+                $"            .d$"       "$$.           ^$r
+               dP            z$$"         ^*$e.          "b
+              4$            e$P             "$$           "
+                           J$F               $$
+                           $$               .$F
+                          4$"               $P"
+                          $"               dP 
+       
+'''
+
+
+menu = '''
+
+
+1. Choco
+2. Exe
+3. Zip
+4. py
+
+
+0. All
+E. Exit
+
+
+'''
+
+
+import ctypes, sys
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+if is_admin():
+    os.system('cls')
+    print(Colorate.Horizontal(Colors.red_to_purple, banner, 1))
+    print('')
+    print(Colorate.Horizontal(Colors.red_to_purple, menu, 1))
+    choco = ['choco', 'install']
+    while True:
+        print('')
+        main_input = Write.Input("Select an option from above > ", Colors.red_to_purple, interval=0.0025).lower()
+        x = main_input.split()
+
+    
+        if main_input == 'choco':
+            os.startfile(f'choco.ps1')
+            with open(f'internal.txt', 'r') as b:
+                for lines in b:
+                    os.system(f'choco install {lines} -y --force')
+
+        elif main_input == 'exe':
+            with open(f'external.txt', 'r') as f:
+                lines = f.readlines()
+                print(f'Installing Your External Files')
+                
+                for line in lines:
+                    url = line
+                    r = requests.get(url, allow_redirects=True)
+                    x = random.random()
+                    open(f'.', 'wb').write(r.content)
+                    
+
+                
+            
+
+
+
+        elif main_input == 'cls':
+            os.system(f'cls')
+            print(Colorate.Horizontal(Colors.red_to_purple, banner, 1))
+            print(Colorate.Horizontal(Colors.red_to_purple, menu, 1))
+
+
+
+        elif main_input == 'clear':
+            os.system(f'cls')
+            print(Colorate.Horizontal(Colors.red_to_purple, banner, 1))
+            print(Colorate.Horizontal(Colors.red_to_purple, menu, 1))
+else:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
